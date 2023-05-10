add56dd249ba86b425e478dad82721d426c20e2d
LeeDongKyu <m3088787@naver.com>
2023-05-10 12:28:11 +0900

    fix HomeWork

---
+++ 19_이동규/session03/Hw02.js
@@ -0,0 +1,13 @@
+let pw = prompt("Enter the password");
+let blank = " ";
+
+if ((pw.length < 6) || (pw.includes(blank)))
+{
+    alert("비밀번호가 너무 짧거나 공백이 있습니다");
+}
+
+else
+{
+    alert("좋은 비밀번호 입니다.");
+}
+
