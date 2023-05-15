644b13e84b133b67e9396256da81d4c6ed1c9c3f
JAR <dkfka13670@naver.com>
2023-05-15 10:24:19 +0900

    submit

--- 23_조아람/session03/Method.js
+++ 23_조아람/session03/Method.js
@@ -1,2 +1,21 @@
-//method 정리//
-abcd.length //문자열의 길이 판단//
+//method 정리
+string.length //문자열의 길이 판단
+string.indexOf(searchvalue, position) //특정 문자의 위치를 찾기
+string.includes( searchString, length ) //문자열이 특정 문자열을 포함하는지 확인
+string.split(separator, limit) //문자열을 일정한 구분자로 잘라서 배열로 저장
+string.substring(indexStart, indexEnd) //문자열 자르기
+string.substr(start, length) //문자열 자르기
+string.slice(beginIndex, endIndex) //문자열 자르기
+string.trim () //문자열 공백을 제거
+string.toUpperCase(); // 문자열을 대문자로 변환하여 주는 함수
+string.toLowerCase(); // 문자열을 소문자로 변환하여 주는 함수
+string.replace("찾을 문자열", "변경할 문자열") //특정 문자를 다른 문자로 치환
+number.Number() //문자열을 숫자로 변환
+number.String () //숫자를 문자열로 변환
+number.parselnt() //문자열을 정수로 바꾸는 함수
+Math.max () //입력값으로 받은 숫자 중 가장 큰 숫자를 반환
+Math.min () //입력값으로 받은 숫자 중 가장 작은 숫자를 반환
+Math.floor () //주어진 숫자와 같거나 작은 정수 중에서 가장 큰 수를 반환
+Math.round() //함수는 입력값을 반올림한 수와 가장 가까운 정수 값을 반환
+Math.random () //난수생성
+Math.sqrt () //함수는 숫자의 제곱근을 반환
