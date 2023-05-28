06ed525b7ae4f5bf7283175c6c69b9100e6e37cf
hansanga <cab08132@naver.com>
2023-05-29 00:03:42 +0900

    과제4

---
+++ 20_한상아/session04/rainbow.js
@@ -0,0 +1,6 @@
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const rainbow = document.getElementsByTagName('span')
+
+for (let i = 0; i < rainbow.length; i++) {
+    rainbow[i].style.color = colors[i];
+}
