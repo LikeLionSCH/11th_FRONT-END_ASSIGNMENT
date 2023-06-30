ae92e185275e50002ecbc6aaa48f502b1cdf0a21
HyeJean <57613101+MEGUMMY1@users.noreply.github.com>
2023-06-30 15:42:00 +0900

    session08 upload

--- 20_조혜진/session08/loginPage.js
+++ 20_조혜진/session08/loginPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Input from "./components/input";
 import styled from "styled-components";
 
 const Div = styled.div`
@@ -17,14 +18,6 @@ const Form = styled.form`
   border-radius: 5px;
 `;
 
-const Input = styled.input`
-  width: 200px;
-  padding: 10px;
-  margin-bottom: 10px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-`;
-
 const StyledButton = styled.button`
   background: orange;
   color: white;
@@ -33,7 +26,7 @@ const StyledButton = styled.button`
   border: none;
 `;
 
-function App() {
+function LoginPage() {
     const [id, setId] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
@@ -59,16 +52,16 @@ function App() {
         <Div>
             <Form onSubmit={submitHandler}>
                 <h1>혜진 로그인</h1>
-                <label>ID</label>
-                <Input type="text" value={id} onChange={changeId} />
-                <label>e-mail</label>
-                <Input type="email" value={email} onChange={changeEmail} disabled />
-                <label>Password</label>
-                <Input type="password" value={password} onChange={changePassword} />
+                <label className="id">ID </label>
+                <Input type="text" className="id" value={id} onChange={changeId}/><br></br>
+                <label className="email">e-mail </label>
+                <Input type="email" value={email} onChange={changeEmail} disabled/><br></br>
+                <label className="pw">P/W </label>
+                <Input type="password" value={password} onChange={changePassword}/>
                 <StyledButton type="submit">Log-in</StyledButton>
             </Form>
         </Div>
     );
 }
     
-export default App;
+export default LoginPage;
