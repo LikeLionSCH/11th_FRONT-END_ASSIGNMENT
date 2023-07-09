9f70836903660aeb66654b15c3648daf21132c8b
hansanga <cab08132@naver.com>
2023-07-10 02:00:14 +0900

    과제8

--- 20_한상아/session08/login.jsx
+++ 20_한상아/session08/login.jsx
@@ -2,6 +2,45 @@ import React, { useState } from "react";
 import Input from "./components/inputs/Input";
 import styled from "styled-components";
 
+const StyledDiv = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+const MyH1 = styled.h1`
+  color: green;
+  text-align: center;
+  font-size: 40px;
+  padding: 0px;
+`;
+
+const MyLabel = styled.label`
+  margin-right: 20px;
+  margin-top: 10px;
+  height: auto;
+`;
+
+const InputDiv = styled.div`
+  width: flex;
+`;
+
+const StyledForm = styled.form`
+    display: flex;
+    width: flex;
+    flex-direction: column;
+    padding: 10px 20px;
+    border: 1px solid #ccc;
+    font-size: 20px;
+`;
+
+const StyledButton = styled.button`
+    background-color: gray;
+    color: white;
+    padding: 10px 15px;
+    border: 1px solid #ccc;
+
+`;
+
 function Login() {
     const [id, setId] = useState("");
     const [email, setEmail] = useState("");
@@ -23,25 +62,6 @@ function Login() {
     const changePassword = (event) => {
         setPassword(event.target.value)
     }
-
-    const StyledDiv = styled.div`
-        display: flex;
-        justify-content: center;
-    `;
-
-    const StyledForm = styled.form`
-        display: flex;
-        flex-direction: column;
-        padding: 10px 20px;
-        border: 1px solid #ccc;
-    `;
-
-    const StyledButton = styled.button`
-        background-color: gray;
-        color: white;
-        padding: 10px 15px;
-        border: 1px solid #ccc;
-    `;
 
     return (
         <StyledDiv>
